1040053af292358f182cd881f21d540e2c41df96
Rupesh Bawane <rupesh.bawane007@gmail.com>
2025-07-29 18:11:06 +0200

    Update KNN.py

--- python/Machine_Learning/KNN.py
+++ python/Machine_Learning/KNN.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
